0160f6a51d71a482ba182a52ed62cefb26a5f59f
Ana Grigalashvili <agrig18@freeuni.edu.ge>
2021-08-03 21:33:49 +0400

    handle exceptions in login

--- app/src/main/java/ge/finalproject/messengerapp/MainActivity.kt
+++ app/src/main/java/ge/finalproject/messengerapp/MainActivity.kt
@@ -27,100 +27,10 @@ class MainActivity : AppCompatActivity() {
 //    private var isAuthMode = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        sharedPref = this.getSharedPreferences(PREFERENCE_FILE, MODE_PRIVATE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initView()
-
-        auth = Firebase.auth
-
-        btnSignIn.setOnClickListener(View.OnClickListener {
-            val nickname: String = etNickname.text.toString()
-            val password: String = etPassword.text.toString()
-
-            if (nickname.isEmpty()){
-                Toast.makeText(this, "Enter nickname!", Toast.LENGTH_SHORT).show()
-                return@OnClickListener
-            }
-
-            if (password.isEmpty()){
-                Toast.makeText(this, "Enter password!", Toast.LENGTH_SHORT).show()
-                return@OnClickListener
-            }
-
-            auth.signInWithEmailAndPassword(nickname + DOMAIN_NAME, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val user = auth.currentUser
-                        // StartActivity monacemebis gadacemit intentshi
-                    } else {
-                        Toast.makeText(baseContext, "Authentication failed",
-                            Toast.LENGTH_SHORT).show()
-                    }
-                }
-        })
-
-        btnGotoSignUp.setOnClickListener(View.OnClickListener() {
-            startActivity(Intent(this, MainActivity::class.java).apply {
-                putExtra(AUTH_MODE, false)
-            })
-        })
-
-        btnSignUp.setOnClickListener(View.OnClickListener() {
-            auth = Firebase.auth;
-            val nickname: String = etNickname.text.toString()
-            val password: String = etPassword.text.toString()
-            val job: String = etJob.text.toString()
-
-            if (nickname.isEmpty()){
-                Toast.makeText(this, "Enter nickname!", Toast.LENGTH_SHORT).show()
-                return@OnClickListener
-            }
-
-            if (password.isEmpty()){
-                Toast.makeText(this, "Enter password!", Toast.LENGTH_SHORT).show()
-                return@OnClickListener
-            }
-
-            if (job.isEmpty()){
-                Toast.makeText(this, "Enter job!", Toast.LENGTH_SHORT).show()
-                return@OnClickListener
-            }
-
-            auth.createUserWithEmailAndPassword(nickname + DOMAIN_NAME, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val user = auth.currentUser
-                        // StartActivity monacemebis gadacemit intentshi
-                    } else {
-                        Toast.makeText(baseContext, "Registration failed",
-                            Toast.LENGTH_SHORT).show()
-                    }
-                }
-        })
-    }
-
-    private fun initView() {
-        etNickname = findViewById(R.id.nickname_et)
-        etPassword = findViewById(R.id.password_et)
-        etJob = findViewById(R.id.job_et)
-        btnSignIn = findViewById(R.id.sign_in_btn)
-        btnGotoSignUp = findViewById(R.id.goto_sign_up_btn)
-        btnSignUp = findViewById(R.id.sign_up_btn)
-        layerAuthorize = findViewById(R.id.authorization_layout)
-        layerRegister = findViewById(R.id.registration_layout)
-
-        if (!(intent.getBooleanExtra(AUTH_MODE, true))){
-            layerAuthorize.visibility = View.GONE
-            layerRegister.visibility = View.VISIBLE
-        }
-    }
-
-    companion object{
-        const val PREFERENCE_FILE = "FILENAME"
-        const val AUTH_MODE = "AUTH_MODE"
-        const val DOMAIN_NAME = "@gmail.com"
+        LoginActivity.start(this)
     }
 
 }
