c55b53cd89cc294373400197903267e989608c38
agrig18 <agrig18@freeuni.edu.ge>
2021-08-04 12:27:44 +0400

    pass values to intent and start activity

--- app/src/main/java/ge/finalproject/messengerapp/HomePageActivity.kt
+++ app/src/main/java/ge/finalproject/messengerapp/HomePageActivity.kt
@@ -1,5 +1,7 @@
 package ge.finalproject.messengerapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,6 +10,17 @@ class HomePageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
+    }
 
+    companion object{
+        private const val USER_NICKNAME = "USER_NICKNAME"
+        private const val USER_PASSWORD = "USER_PASSWORD"
+
+        fun start(context: Context, nickname: String, password: String){
+            context.startActivity(Intent(context, HomePageActivity::class.java).apply {
+                putExtra(USER_NICKNAME, nickname)
+                putExtra(USER_PASSWORD, password)
+            })
+        }
     }
 }
