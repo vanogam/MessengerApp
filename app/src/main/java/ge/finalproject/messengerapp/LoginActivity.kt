374e178d737fb03055408f55e3293e7b3c7e0863
agrig18 <agrig18@freeuni.edu.ge>
2021-08-04 14:22:10 +0400

    call respective startActivity methods

--- app/src/main/java/ge/finalproject/messengerapp/LoginActivity.kt
+++ app/src/main/java/ge/finalproject/messengerapp/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -56,7 +55,7 @@ class LoginActivity : AppCompatActivity() {
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         val user = auth.currentUser
-                        HomePageActivity.start(this, nickname, password)
+                        HomePageActivity.startFromAuthorization(this)
                     } else {
                         try {
                             throw task.exception!!
@@ -100,7 +99,7 @@ class LoginActivity : AppCompatActivity() {
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         val user = auth.currentUser
-                        HomePageActivity.start(this, nickname, password)
+                        HomePageActivity.startFromRegistration(this, nickname, password, job)
                     } else {
                         try {
                             throw task.exception!!
